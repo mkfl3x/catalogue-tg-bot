f35c4c5e2e4f9dc62f2235c89185e6ac8b5f39b7
Mikhail Kuznetsov <mkfl3x@gmail.com>
2021-10-16 01:04:06 +0300

    sending keyboard action

--- src/main/kotlin/bot/BotActions.kt
+++ src/main/kotlin/bot/BotActions.kt
@@ -1,11 +1,16 @@
 package bot
 
 import com.pengrad.telegrambot.TelegramBot
+import com.pengrad.telegrambot.model.request.Keyboard
 import com.pengrad.telegrambot.request.SendMessage
 
 class BotActions(private val bot: TelegramBot) {
 
-    fun sendMessage(chatId: Long, messageText: String) {
+    fun sendTextMessage(chatId: Long, messageText: String) {
         bot.execute(SendMessage(chatId, messageText))
+    }
+
+    fun sendKeyboard(chatId: Long, title:String, keyboard: Keyboard) {
+        bot.execute(SendMessage(chatId, title).replyMarkup(keyboard))
     }
 }
