72f3b309cc80fb9cc9c91d26a51c1c31545d331a
Mikhail Kuznetsov <mkfl3x@gmail.com>
2021-10-24 03:49:15 +0300

    mongo client updates

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,5 @@
 import server.Server
 
 fun main() {
-
-    // TODO:
-    //  - add logs
-    //  - add mongo db
-    //  - add dockerfile
-    //  - add admin panel (react?)
-    //  - add messages handling
-
     Server().start()
 }
--- src/main/kotlin/database/MongoClient.kt
+++ src/main/kotlin/database/MongoClient.kt
@@ -1,5 +1,6 @@
 package database
 
+import com.mongodb.BasicDBObject
 import com.mongodb.MongoClient
 import com.mongodb.MongoClientSettings
 import org.bson.codecs.configuration.CodecRegistries
@@ -21,11 +22,24 @@ object MongoClient {
     private val database = client.getDatabase(Properties.get("mongo.database"))
         .withCodecRegistry(codecs)
 
-    fun <T> insert(collection: String, entry: T, entryType: Class<T>) {
+    fun <T> create(collection: String, entry: T, entryType: Class<T>) {
         database.getCollection(collection, entryType).insertOne(entry)
     }
 
-    fun <T> read(collection: String, entryType: Class<T>): List<T> {
+    fun <T> readAllEntries(collection: String, entryType: Class<T>): List<T> {
         return database.getCollection(collection, entryType).find().toList()
+    }
+
+    fun <T> read(collection: String, entryType: Class<T>, condition: BasicDBObject): T {
+        return database.getCollection(collection, entryType).find(condition).first()
+    }
+
+    fun update() {
+        // TODO
+    }
+
+    fun delete(collection: String, condition: BasicDBObject) {
+        database.getCollection(collection).deleteOne(condition)
+        // TODO: add log entry
     }
 }
