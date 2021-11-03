8faed40c7c087d2d80868202489c070c50be7726
Mikhail Kuznetsov <mkfl3x@gmail.com>
2021-11-04 01:01:53 +0300

    add keyboard request model

--- src/main/kotlin/server/RequestModels.kt
+++ src/main/kotlin/server/RequestModels.kt
@@ -1,6 +1,13 @@
 package server
 
 import database.Button
+import database.Keyboard
+
+data class AddKeyboardRequest(
+    val keyboardName: String,
+    val newButton: String,
+    val keyboard: Keyboard
+)
 
 data class AddButtonRequest(
     val keyboard: String,
